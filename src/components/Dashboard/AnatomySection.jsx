6a6053db9232748e867817010374570ef3e6c7b2
irfanzmohammed <irfanmuhammed877@gmail.com>
2025-05-28 00:39:40 +0530

    Initial dashboard setup

--- src/components/Dashboard/AnatomySection.jsx
+++ src/components/Dashboard/AnatomySection.jsx
@@ -1,15 +1,16 @@
-import React from 'react'
+import React from 'react';
 import { ZoomIn } from 'lucide-react';
+import humanbody from '../../assets/humanbody.png';
 
 const AnatomySection = () => {
   return (
-    <div className='anatomy-box'>
-       <ZoomIn className='zoom-icon' size={16}/>
-      <img src='src/assets/humanbody.png' alt='humanbody' className='humanbody'/>
+    <div className="anatomy-box">
+      <ZoomIn className="zoom-icon" size={16} />
+      <img src={humanbody} alt="humanbody" className="humanbody" />
       <div className="health-label heart">❤️ Healthy Heart</div>
       <div className="health-label leg">🦵 Healthy Leg</div>
     </div>
-  )
-}
+  );
+};
 
-export default AnatomySection
+export default AnatomySection;
